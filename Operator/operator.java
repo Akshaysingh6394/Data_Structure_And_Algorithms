2de47ce6dba32490380b032824ab92ed9c932d02
akshaysingh6394 <aksc933604c8@gmail.com>
2023-12-27 13:48:27 +0530

    OP

---
+++ Operator/operator.java
@@ -0,0 +1,9 @@
+public class operator {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 7;
+        System.out.println(A+B);
+        System.out.println(A/B);
+        System.out.println(A*B);
+    }
+}
