7a2fe5acc230451bda4f43de48b2b68008935405
akshaysingh6394 <aksc933604c8@gmail.com>
2023-12-27 13:36:16 +0530

    Operators

---
+++ Operator/Unary.java
@@ -0,0 +1,10 @@
+public class Unary {
+    public static void main(String[] args) {
+        int a = 7;
+        //int b = --a; output is 6
+        int b = a--;
+        System.out.println(b);//7
+    
+    }
+    
+}
