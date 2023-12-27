aa8c2322e9560511c83d31e9d2e23726e44b1af1
akshaysingh6394 <aksc933604c8@gmail.com>
2023-12-27 13:45:32 +0530

    firstCommit

--- Operator/Unary.java
+++ Operator/Unary.java
@@ -2,6 +2,7 @@ public class Unary {
     public static void main(String[] args) {
         int a = 7;
         //int b = --a; output is 6
+        //kjkvjfk
         int b = a--;
         System.out.println(b);//7
     
