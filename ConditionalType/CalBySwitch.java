0240a529c37324e12f9fde76808d39a89ab52088
akshaysingh6394 <aksc933604c8@gmail.com>
2023-12-28 23:32:28 +0530

    Conditional

--- ConditionalType/CalBySwitch.java
+++ ConditionalType/CalBySwitch.java
@@ -1,7 +1,31 @@
 package ConditionalType;
-
+import java.util.*;
 public class CalBySwitch {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter value a: ");
+        int A = sc.nextInt();
+        System.out.println("Enter value B: ");
+        int B = sc.nextInt();
+        System.out.println("Choose Operator: ");
+        char operator = sc.next().charAt(0);
+        switch(operator){
+            case '+' : System.out.println(A+B);
+            break;
+            case '*' : System.out.println(A*B);
+            break;
+            case '/' : System.out.println(A/B);
+            break;
+            case '%' : System.out.println(A%B);
+            break;
+            case '-' : System.out.println(A-B);
+            break;
+            default : System.out.println("operator not valid");
+            break;
+
+        }
+
+
         
     }
     
