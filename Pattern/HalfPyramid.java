62ceafd81b2fb8ffaa16b0e362918f39a7ecb4d5
akshaysingh6394 <aksc933604c8@gmail.com>
2023-12-30 14:44:18 +0530

    Pattern

---
+++ Pattern/HalfPyramid.java
@@ -0,0 +1,12 @@
+public class HalfPyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+                
+            }System.out.println();
+            
+        }
+    }
+    
+}
