e0d258335dae12ef429336447fc65ab7e9cd2f64
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-09 15:06:42 +0530

    Array

--- Array/pairs.java
+++ Array/pairs.java
@@ -2,7 +2,7 @@ public class pairs {
     public static void pairs(int num[]){
         for (int i = 0; i < num.length; i++) {
             for (int j = i+1; j < num.length; j++) {
-                System.out.println(num[i]+" "+num[j]);
+                System.out.print("("+num[i]+" "+num[j]+")");
                
             }   
             System.out.println();
