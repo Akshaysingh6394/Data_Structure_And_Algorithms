caf82ccc1341175334f889d337e9eead19536aef
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-09 15:11:50 +0530

    cont of pairs

--- Array/pairs.java
+++ Array/pairs.java
@@ -1,13 +1,17 @@
 public class pairs {
     public static void pairs(int num[]){
+        int tp = 0;
         for (int i = 0; i < num.length; i++) {
             for (int j = i+1; j < num.length; j++) {
                 System.out.print("("+num[i]+" "+num[j]+")");
+                tp++;
                
             }   
             System.out.println();
+          
             
         }
+          System.out.println(tp);
     }
     public static void main(String[] args) {
         int num[] = {2,4,6,8,10};
