766effdd04e84176f1788e37974d10743baeb74f
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-10 23:09:56 +0530

    insertion

--- Sorting/insertion.java
+++ Sorting/insertion.java
@@ -1,5 +1,28 @@
 package Sorting;
 
 public class insertion {
-    
+    public static void insertSort(int arr[]){
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev --;
+            }
+            arr[prev+1] = curr;
+        }
+        
+    }
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        insertSort(arr);
+        printArr(arr);
+    }
 }
