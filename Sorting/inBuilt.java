5cfd0ef98110b5da3d5c65541cb827a1fbdc5ad8
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-11 11:52:35 +0530

    Bajpeyi

--- Sorting/inBuilt.java
+++ Sorting/inBuilt.java
@@ -2,10 +2,16 @@ package Sorting;
 import java.util.Arrays;
 
 public class inBuilt {
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
         Arrays.sort(arr);
-        System.out.println(arr);
+        printArr(arr);
         
     }
     
