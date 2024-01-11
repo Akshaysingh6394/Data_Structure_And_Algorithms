de7adda2cafdb45548431790cdf9be5beff48df7
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-11 11:34:01 +0530

    Bajpayi

---
+++ Sorting/inBuilt.java
@@ -0,0 +1,12 @@
+package Sorting;
+import java.util.Arrays;
+
+public class inBuilt {
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        Arrays.sort(arr);
+        System.out.println(arr);
+        
+    }
+    
+}
