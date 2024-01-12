46e239448806df8327d58074bdabafcf47c64d50
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-13 01:50:39 +0530

    Sorting

--- Sorting/inBuilt.java
+++ Sorting/inBuilt.java
@@ -6,7 +6,8 @@ public class inBuilt {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
-        System.out.println();
+        System.out.println();// for next line
+        
     }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
