14fcf19b0ee99d9eb18dfc20d544d9b0611385cc
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-16 12:34:00 +0530

    String

---
+++ String/StringBuil.java
@@ -0,0 +1,13 @@
+package String;
+public class StringBuil {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch = 'a';ch <= 'z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.toString());
+
+    }
+    
+}
