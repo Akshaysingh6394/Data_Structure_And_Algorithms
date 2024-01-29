52cff70ac6585ef792a9d3d90271d768572ba317
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-29 22:39:11 +0530

    Arraylist

--- ArrayList/create.java
+++ ArrayList/create.java
@@ -4,7 +4,16 @@ import java.util.*;
 public class create {
     public static void main(String[] args) {
         ArrayList <Integer> list = new ArrayList<>();
-        list.add
+        list.add(2);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        System.out.println(list.get(1));
+        list.set(2, 9);
+        System.out.println(list);
+        
         
     }
     
