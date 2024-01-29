3d7bc9141959408c709043c4baa1ef62875e1c11
akshaysingh6394 <aksc933604c8@gmail.com>
2024-01-29 22:31:18 +0530

    Arraylist

---
+++ ArrayList/create.java
@@ -0,0 +1,11 @@
+package ArrayList;
+import java.util.*;
+
+public class create {
+    public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add
+        
+    }
+    
+}
