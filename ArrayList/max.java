c275d6d3e34d5d2d8388a0795e321563c4ed25a6
akshaysingh6394 <singhakshay8081@gmail.com>
2024-02-04 00:27:28 +0530

    maximum element in arraylist

---
+++ ArrayList/max.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class max {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(9);
+        list.add(5);
+        list.add(4);
+        list.add(6);
+        list.add(12);
+        System.out.println(list);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i)>max){
+                max = list.get(i);
+
+            }
+            
+        }
+        System.out.println(max);
+
+    }
+    
+}
---
+++ ArrayList/swap.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class swap {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(9);
+        list.add(5);
+        list.add(4);
+        list.add(6);
+        list.add(12);
+        System.out.println(list);
+
+    }
+    
+}
