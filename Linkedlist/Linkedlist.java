4b0c3b74f97985115f5a547c11834d3b5e8dac76
akshaysingh6394 <singhakshay8081@gmail.com>
2024-02-12 02:34:03 +0530

    Linkedlist

---
+++ Linkedlist/Linkedlist.java
@@ -0,0 +1,18 @@
+
+public class Linkedlist{
+    public static class Node{
+        int data;
+        Node Next;
+        public Node(int data){
+            this.data = data;
+            this.Next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Linkedlist l1 = new Linkedlist();
+        l1.head = new Node(1);
+        li.head.Next = new Node(2);
+        
+        
+    }
+}
