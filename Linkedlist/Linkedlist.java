c4729fe604ea8e250cba95a838108dab089efffd
akshaysingh6394 <singhakshay8081@gmail.com>
2024-03-03 23:06:00 +0530

    LinkedList

--- Linkedlist/Linkedlist.java
+++ Linkedlist/Linkedlist.java
@@ -8,11 +8,52 @@ public class Linkedlist{
             this.Next = null;
         }
     }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.Next = head;
+        head = newNode;
+
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.Next = newNode;
+        tail = newNode.Next;
+    }
+    public void print(){
+        if(head == null){
+            System.out.println("Empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data+ "-->");
+            temp = temp.Next;
+
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Linkedlist l1 = new Linkedlist();
-        l1.head = new Node(1);
-        li.head.Next = new Node(2);
-        // new 
+        l1.print();
+        l1.addFirst(1);
+        l1.print();
+        l1.addFirst(2);
+        l1.print();
+        l1.addLast(3);
+        l1.print();
+        l1.addLast(4);
+        l1.print();
+       
         
         
     }
