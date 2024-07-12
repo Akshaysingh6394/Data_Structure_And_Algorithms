bfa9a362615615a3ffab2fe52c0f79d116ac984f
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-13 01:02:55 +0530

    Reverse a string

---
+++ Stack/revStr.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class revStr {
+    public static String reverseString(Sring str){
+        Stack<Character> s = new Stack<>();
+
+        int idx =0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String str = "AKshay";
+        String result = reverseString(Str);
+        System.out.println(result);
+    }
+    
+    
+}
