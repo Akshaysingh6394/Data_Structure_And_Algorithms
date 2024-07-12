b798e287fefd2f999774962adbb185e57dff024d
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-12 22:16:38 +0530

    push at bottom

---
+++ Stack/pushBot.java
@@ -0,0 +1,26 @@
+package Stack;
+import java.util.*;
+public class pushBot {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+
+    }
+    public static void main(String[] args) {
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushAtBottom(s,4);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+    }
+    
+}
